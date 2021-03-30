2a77264d0ce11a900ae85b1a1c3d699d4b00fe48
cmattnielsen <cmattnielsen@gmail.com>
2021-03-29 20:50:45 -0600

    Finished

--- Exam_2/NIELSEN_Skills_Test_2.R
+++ Exam_2/NIELSEN_Skills_Test_2.R
@@ -69,7 +69,8 @@ mutate(unicef,numeric.as())
 
 as.numeric(as.character(unicef))
 
-
+df2 <- pivat_longer(starts_with("U5MR"),
+                    names_to
 
 
 unicef2 <- unicef %>%
@@ -95,6 +96,83 @@ df$unicef
 
 df <- unicef %>%
   mutate(unicef = replace(unicef, is.na(unicef), "unavailable"))
+
+
+
+
+# clean unicef the right way
+
+
+DFunicef <- unicef %>% 
+  pivot_longer(starts_with("U5MR"),
+               names_to="Year",
+               values_to="MortalityRate",
+               names_prefix="U5MR.") %>%
+  mutate(Year=as.numeric(Year),
+         MortalityRate=as.numeric(MortalityRate))
+
+
+
+DFunicef %>% 
+  ggplot(aes(x=Year,y=MortalityRate,color=Region)) +
+  geom_smooth()
+
+
+
+#fig 2
+ggplot(DFunicef,aes(x=Year,y=MortalityRate,color=Continent)) +
+  geom_point(size=2.5) +
+  ggsave("NIELSEN_Fig_2.jpg")
+
+
+
+
+#fig 3
+DFunicef %>%
+  group_by(Continent,Year) %>%
+  summarise(MeanMR= mean(MortalityRate,na.rm = TRUE)) %>%
+  ggplot(aes(x=Year,y=MeanMR,color=Continent,group=Continent)) +
+  geom_line(size=3) +
+  ggsave("NIELSEN_Fig_3.jpg")
+
+
+
+#fig 4
+DFunicef %>%
+  ggplot(aes(x=Year,y=MortalityRate)) +
+  geom_point(color="Blue",shape=1,alpha=1) +
+  facet_wrap(~Region) +
+  ggsave("NIELSEN_Fig_4.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
